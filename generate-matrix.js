3fb01fd5f679c9638e3fbc0b8cfe4838ae2d8280
Fabricio Parola <fabricio.parola@gmail.com>
2020-04-23 23:16:18 -0300

    feat: add module to generate random matrix

---
+++ generate-matrix.js
@@ -0,0 +1,19 @@
+class GenerateMatrixUtil {
+    static random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    static generate({ width = 5, height = 5, min = 1, max = 10 } = {}) {
+        let mtx = []
+        for (let i = 0; i < width; i++) {
+            for (let j = 0; j < height; j++) {
+                mtx[i] = mtx[i] || []
+                mtx[i][j] = this.random(min, max)
+            }
+        }
+        return mtx
+    }
+}
+
+const mtx = GenerateMatrixUtil.generate({ max: 100, width: 100, height: 100 })
+console.log(mtx)
